7c9d9fb83632688e604b299b6cf1e190e6249c8c
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-04-02 01:29:27 -0400

    added functionality to start button

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 //=================
 
-var quizQuestionBank;
+var questionTwo = [
+  "The condition in an if / else statement is enclosed with ___________.",
+];
+var answerTwo = ["quotes", "curly brackets", "parentheses", "square brackets"];
+var quizQuestionBank = [];
+var answerBank = [];
+
+var nextQuestion = document.querySelector("#start-quiz");
+var beforeQuizContent = document.querySelector(".before-quiz-content");
+var quizCardContent = document.querySelector(".quiz-card-content");
+nextQuestion.addEventListener("click", startQuiz);
+
+function startQuiz() {
+  //we need the intro stuff to go away
+  beforeQuizContent.style.display = "none";
+  quizCardContent.style.display = "flex";
+  console.log("yay");
+}
